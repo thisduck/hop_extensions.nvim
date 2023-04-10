1dca4445b5176491a177174d6fc45cec0f5541ea
Adnan Ali <adnan.ali@gmail.com>
2023-04-10 12:51:53 -0400

    Update to use latest hint_char1.

--- lua/hop_extensions/hint_char1.lua
+++ lua/hop_extensions/hint_char1.lua
@@ -1,24 +1,32 @@
-local jump_target = require("hop.jump_target")
 local hop = require("hop")
 
 local M = {}
 M.opts = {}
 
 function M.hint_char1(opts)
-	opts = nil
+	opts = M.opts or {}
+	local jump_target = require 'hop.jump_target'
 
-	local ok, c = pcall(vim.fn.getchar)
-	if not ok then
+	local c = hop.get_input_pattern('Hop 1 char: ', 1)
+	if not c then
 		return
 	end
 
-	local generator = jump_target.jump_targets_by_scanning_lines
+	local generator
+	if opts.current_line_only then
+		generator = jump_target.jump_targets_for_current_line
+	else
+		generator = jump_target.jump_targets_by_scanning_lines
+	end
 
-	if c == 32 then
+	if c == ' ' then
 		return hop.hint_with(generator(jump_target.regex_by_searching("\\v\\S\\zs ")), opts)
 	end
 
-	hop.hint_with(generator(jump_target.regex_by_case_searching(c, true, opts)), opts)
+	hop.hint_with(
+		generator(jump_target.regex_by_case_searching(c, true, opts)),
+		opts
+	)
 end
 
 function M.register(opts)
