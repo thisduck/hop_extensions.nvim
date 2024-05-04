f03a60ddab03a6a4502c9910c8a8053e726a04b2
Adnan Ali <adnan.ali@gmail.com>
2024-05-04 13:25:48 -0400

    Updating for latest hop fork.

--- lua/hop_extensions/hint_char1.lua
+++ lua/hop_extensions/hint_char1.lua
@@ -1,36 +1,25 @@
 local hop = require("hop")
 
 local M = {}
-M.opts = {}
 
 function M.hint_char1(opts)
-	opts = M.opts or {}
-	local jump_target = require 'hop.jump_target'
+	local jump_regex = require('hop.jump_regex')
+
+	opts = hop.opts
 
 	local c = hop.get_input_pattern('Hop 1 char: ', 1)
 	if not c then
 		return
 	end
 
-	local generator
-	if opts.current_line_only then
-		generator = jump_target.jump_targets_for_current_line
-	else
-		generator = jump_target.jump_targets_by_scanning_lines
-	end
-
 	if c == ' ' then
-		return hop.hint_with(generator(jump_target.regex_by_searching("\\v\\S\\zs ")), opts)
+		hop.hint_with_regex(jump_regex.regex_by_case_searching("\\v\\S\\zs ", false, opts), opts)
+	else
+		hop.hint_with_regex(jump_regex.regex_by_case_searching(c, true, opts), opts)
 	end
-
-	hop.hint_with(
-		generator(jump_target.regex_by_case_searching(c, true, opts)),
-		opts
-	)
 end
 
 function M.register(opts)
-	M.opts = opts
 end
 
 return M
